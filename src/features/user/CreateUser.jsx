eb0fc9d2d8d2e6a4873378deecbb860014ecb28a
lenienguyen <lenie.nguy10@gmail.com>
2025-07-27 16:48:46 +0200

    feat(styling): add tailwind classes

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const CreateUser = () => {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -9,16 +9,19 @@ const CreateUser = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
-      {username !== "" && (
+      {username !== '' && (
         <div>
           <button>Start ordering</button>
         </div>
